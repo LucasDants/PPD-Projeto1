c32c31dcf9773073e1be6f4557187f2c73b401dd
Lucas Dantas <55062200+LucasDants@users.noreply.github.com>
2024-11-04 05:16:31 -0600

    refactor: use createServer from node

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,3 +1,10 @@
-import fastify from 'fastify'
+import { createServer } from 'http';
+import { Server } from 'socket.io';
 
-export const app = fastify()
+export const app = createServer()
+
+export const io = new Server(app, {
+  cors: {
+    origin: 'http://localhost:5173'
+  }
+})
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,3 +1,53 @@
-import { app } from "./app";
+import { app, io } from "./app";
 
-app.listen({ port: 3333, host: '0.0.0.0' }).then(() => console.log("🚀 HTTP Server is running on port 3333"));
+type Room = {
+  id: string
+  players: [string, string] | [string]
+}
+
+const rooms: Room[] = []
+
+io.use((socket, next) => {
+  const roomId: string = socket.handshake.auth.roomId ?? ''
+  const sessionId: string = socket.handshake.auth.sessionId ?? ''
+
+  if (sessionId === '') {
+    return next(new Error("Invalid User"))
+  }
+
+  if (roomId === '') {
+    return next(new Error("Invalid Room"))
+  }
+
+  let room = rooms.find(room => room.id === roomId)
+
+  if (room != null) {
+    if (room.players.length === 2) {
+      if (!room.players.includes(sessionId)) {
+        return next(new Error("Room is full"))
+      }
+    }
+
+    if (room.players.length < 2) {
+      room.players.push(sessionId)
+    }
+
+  } else {
+    rooms.push({
+      id: roomId,
+      players: [sessionId]
+    })
+  }
+
+  socket.roomId = roomId
+  socket.sessionId = sessionId
+
+  return next()
+})
+
+io.on('connection', socket => {
+  console.log(socket.handshake.auth)
+  console.log(rooms)
+})
+
+app.listen({ port: 3333, host: '0.0.0.0' }, () => console.log("🚀 HTTP Server is running on port 3333"))
